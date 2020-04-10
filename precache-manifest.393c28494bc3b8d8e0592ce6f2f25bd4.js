c39db5ab8568d8211b9a2e4a0f7ab9340e7e91d5
Ivan Shevchenko <rayserviz@gmail.com>
2020-04-10 17:12:40 +0300

    Updates

---
+++ precache-manifest.393c28494bc3b8d8e0592ce6f2f25bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be64702e46526f1dedc9ee39b79996bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "006f1ffe3fa4089aa54d",
+    "url": "/static/css/main.08f4667d.chunk.css"
+  },
+  {
+    "revision": "e7580c7c27050e01b258",
+    "url": "/static/js/2.32708735.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.32708735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006f1ffe3fa4089aa54d",
+    "url": "/static/js/main.c6f610f8.chunk.js"
+  },
+  {
+    "revision": "cef00bd93c6092ac900c",
+    "url": "/static/js/runtime-main.ca31bebc.js"
+  }
+]);
